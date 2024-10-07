6edb6c59b67cf130844b2bcfd078c5b4f86058d8
Donald Williams <129223418+dwilliam62@users.noreply.github.com>
2024-10-06 22:15:10 -0400

    Added nwg-dock style.css and startup script
    Signed-off-by: Donald Williams <129223418+dwilliam62@users.noreply.github.com>
    
     On branch Development
     Your branch is up to date with 'origin/Development'.
    
     Changes to be committed:
    	modified:   CHANGELOG.ddubs
    	new file:   assets/dotfiles/nwg-dock-hyprland/start.nwg.dock.sh
    	new file:   assets/dotfiles/nwg-dock-hyprland/style.css

---
+++ assets/dotfiles/nwg-dock-hyprland/start.nwg.dock.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+# Start nwg-dock-hyprland with my settings 
+# smaller icons, bottom screen wth border, start button on left running rofi menu
+
+ nwg-dock-hyrpaldn  -i 32 -w 10 -mb 10 -x -c "rofi -show drun"  -lp start  
