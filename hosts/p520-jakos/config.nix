27ae73a0c2e1bd7abd879eb17bdf7d012aaa90be
Donald Williams <129223418+dwilliam62@users.noreply.github.com>
2024-10-11 11:36:25 -0400

    Added packages.nix to p520 host as import pgms to all host
    Signed-off-by: Donald Williams <129223418+dwilliam62@users.noreply.github.com>
    
     On branch Development
     Your branch is up to date with 'origin/Development'.
    
     Changes to be committed:
    	modified:   CHANGELOG.ddubs
    	modified:   hosts/p520-jakos/config.nix
    	renamed:    hosts/program-list-backup.txt -> hosts/programs.list
    	modified:   modules/packages.nix
    	renamed:    hosts/packages.list -> modules/packages.nix.bad

--- hosts/p520-jakos/config.nix
+++ hosts/p520-jakos/config.nix
@@ -21,7 +21,7 @@
     ../../modules/intel-drivers.nix
     ../../modules/vm-guest-services.nix
     ../../modules/local-hardware-clock.nix
-    #../../modules/packages.nix  Broken 
+    ../../modules/packages.nix  
    
   ];
 
@@ -255,109 +255,6 @@
     wlogout
     yad
     yt-dlp
-
-     # My programs
-    (inputs.wezterm.packages.${pkgs.system}.default)
-    alacritty                                                                             
-    appimage-run                                                                          
-    aquamarine                                                                            
-    atop                                                                                  
-    bat                                                                                   
-    bottom                                                                                
-    brightnessctl                                                                         
-    clang                                                                                 
-    clang-tools 
-    cmatrix                                                                               
-    cpufetch                                                                              
-    cpuid                                                                                 
-    cpu-x
-    dig                                                                                   
-    discord
-    dua                                                                                   
-    figlet                                                                                
-    flameshot
-    freeoffice
-    foot                                                                                  
-    # fish shell and plugins
-    fish
-    fishPlugins.done
-    fishPlugins.fzf
-    fishPlugins.forgit
-    fishPlugins.hydro
-    fishPlugins.bass
-    fzf
-    fishPlugins.grc
-    grc
-    #  End
-    gcc        
-    git
-    github-desktop
-    glances                                                                               
-    google-chrome                                                                         
-    gotop
-    gping
-    gsmartcontrol
-    gtop
-    htop                                                                                  
-    hyfetch 
-    hyprlang                                                                              
-    hyprshot                                                                              
-    hyprutils                                                                             
-    imagemagick
-    iotop
-    ipfetch
-    jellyfin-media-player                                                                 
-    lazygit                                                                               
-    libvirt
-    light                                                                                 
-    lm_sensors
-    lolcat                                                                                
-    mc
-    meld
-    meson 
-    mission-center                                                                        
-    ncdu                                                                                  
-    ncftp                                                                                 
-    nodejs_22
-    neofetch                                                                              
-    neovide                                                                               
-    neovim                                                                                
-    nh                                                                                    
-    nomachine-client                                                                      
-    nomacs                                                                                
-    nvtopPackages.full 
-    nwg-drawer
-    nwg-dock-hyprland
-    nwg-launchers
-    obs-studio
-    okular                                                                                
-    ouch
-    ptyxis
-    powertop
-    putty                                                                                 
-    quickemu                                                                              
-    #quickgui                                                                             
-    ripgrep
-    smartmontools
-    socat                                                                                 
-    stacer                                                                                
-    tmux                                                                                  
-    ugrep                                                                                 
-    unrar                                                                                 
-    v4l-utils
-    vesktop                                                                               
-    virt-manager                                                                          
-    virt-viewer                                                                           
-    vlc                                                                                   
-    vscode-fhs
-    w3m
-    warp-terminal
-    wf-recorder                                                                           
-    yad
-    yazi                                                                                  
-    zellij                                                                                
-    zig                                                                                   
-    zoxide                                                                               
 
     #waybar  # if wanted experimental next line
     #(pkgs.waybar.overrideAttrs (oldAttrs: { mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];}))
--- modules/packages.nix
+++ modules/packages.nix
@@ -1,107 +1,114 @@
- pkgs, ... }:
+{ pkgs, inputs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
 
 
-# define packages here
- environment.systemPackages = (with pkgs; [
+     # My programs
+    (inputs.wezterm.packages.${pkgs.system}.default)
+    alacritty                                                                             
+    appimage-run                                                                          
+    aquamarine                                                                            
+    atop                                                                                  
+    bat                                                                                   
+    bottom                                                                                
+    brightnessctl                                                                         
+    clang                                                                                 
+    clang-tools 
+    cmatrix                                                                               
+    cpufetch                                                                              
+    cpuid                                                                                 
+    cpu-x
+    dig                                                                                   
+    discord
+    dua                                                                                   
+    figlet                                                                                
+    flameshot
+    freeoffice
+    foot                                                                                  
+    # fish shell and plugins
+    fish
+    fishPlugins.done
+    fishPlugins.fzf
+    fishPlugins.forgit
+    fishPlugins.hydro
+    fishPlugins.bass
+    fzf
+    fishPlugins.grc
+    grc
+    #  End
+    gcc        
+    git
+    github-desktop
+    glances                                                                               
+    google-chrome                                                                         
+    gotop
+    gping
+    gsmartcontrol
+    gtop
+    htop                                                                                  
+    hyfetch 
+    hyprlang                                                                              
+    hyprshot                                                                              
+    hyprutils                                                                             
+    imagemagick
+    iotop
+    ipfetch
+    jellyfin-media-player                                                                 
+    lazygit                                                                               
+    libvirt
+    light                                                                                 
+    lm_sensors
+    lolcat                                                                                
+    mc
+    meld
+    meson 
+    mission-center                                                                        
+    ncdu                                                                                  
+    ncftp                                                                                 
+    nodejs_22
+    neofetch                                                                              
+    neovide                                                                               
+    neovim                                                                                
+    nh                                                                                    
+    nomachine-client                                                                      
+    nomacs                                                                                
+    nvtopPackages.full 
+    nwg-drawer
+    nwg-dock-hyprland
+    nwg-launchers
+    obs-studio
+    okular                                                                                
+    ouch
+    ptyxis
+    powertop
+    putty                                                                                 
+    quickemu                                                                              
+    #quickgui                                                                             
+    ripgrep
+    smartmontools
+    socat                                                                                 
+    stacer                                                                                
+    tmux                                                                                  
+    ugrep                                                                                 
+    unrar                                                                                 
+    v4l-utils
+    vesktop                                                                               
+    virt-manager                                                                          
+    virt-viewer                                                                           
+    vlc                                                                                   
+    vscode-fhs
+    w3m
+    warp-terminal
+    wf-recorder                                                                           
+    yad
+    yazi                                                                                  
+    zellij                                                                                
+    zig                                                                                   
+    zoxide                                                                               
 
-(inputs.wezterm.packages.${pkgs.system}.default)
-alacritty                                                                             
-appimage-run                                                                          
-aquamarine                                                                            
-atop                                                                                  
-bat                                                                                   
-bottom                                                                                
-brightnessctl                                                                         
-clang                                                                                 
-clang-tools 
-cmatrix                                                                               
-cpufetch                                                                              
-cpuid                                                                                 
-cpu-x
-dig                                                                                   
-discord
-dua                                                                                   
-figlet                                                                                
-foot                                                                                  
-# fish shell and plugins
-fish
-fishPlugins.done
-fishPlugins.fzf
-fishPlugins.forgit
-fishPlugins.hydro
-fishPlugins.bass
-fzf
-fishPlugins.grc
-grc
-#  End
-gcc        
-gearlever                                                                             
-git
-glances                                                                               
-google-chrome                                                                         
-gotop
-gping
-gsmartcontrol
-gtop
-htop                                                                                  
-hyfetch 
-hyprlang                                                                              
-hyprshot                                                                              
-hyprutils                                                                             
-mc
-iotop
-ipfetch
-jellyfin-media-player                                                                 
-jitsi
-jitsi-meet
-lazygit                                                                               
-libvirt
-light                                                                                 
-lm_sensors
-lolcat                                                                                
-meld
-meson 
-mission-center                                                                        
-ncdu                                                                                  
-ncftp                                                                                 
-modejs_22
-neofetch                                                                              
-neovide                                                                               
-neovim                                                                                
-nh                                                                                    
-nomachine-client                                                                      
-nomacs                                                                                
-nvtopPackages.full 
-nwg-drawer
-nwg-launchers
-obs-studio
-okular                                                                                
-ouch
-powertop
-putty                                                                                 
-quickemu                                                                              
-#quickgui                                                                             
-ripgrep
-smartmontools
-socat                                                                                 
-stacer                                                                                
-tmux                                                                                  
-ugrep                                                                                 
-unrar                                                                                 
-v4l-utils
-vesktop                                                                               
-virt-manager                                                                          
-virt-viewer                                                                           
-vlc                                                                                   
-vscode-with-extensions
-w3m
-warp-terminal
-wf-recorder                                                                           
-yad
-yazi                                                                                  
-zellij                                                                                
-zig                                                                                   
-zoxide                                                                                
-      
-      ];
-   };
+
+
+  ];
+}
+
