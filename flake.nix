51d5b25cdb75aee7077184de3a97ac00c87aa91f
installer <installer@gmail.com>
2024-10-12 00:43:09 -0400

    Updated flake

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
 	inputs@{ self,nixpkgs, wezterm,  ... }:
     	let
       system = "x86_64-linux";
-      host = "p520-jakos";
+      host = "prometheus";
       username = "dwilliams";
        defaultPackage.x86_64-linux = wezterm.packages.x86_64-linux.default;
 
