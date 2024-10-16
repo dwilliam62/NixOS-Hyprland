c1af537daecc81a6c23c897dcbb3de5b50c328ab
Donald Williams <129223418+dwilliam62@users.noreply.github.com>
2024-10-16 13:52:39 -0400

    cleand up overwrite and added comments and PR tracker
    Signed-off-by: Donald Williams <129223418+dwilliam62@users.noreply.github.com>

--- modules/packages.nix
+++ modules/packages.nix
@@ -6,25 +6,20 @@
 
      # My programs
     (inputs.wezterm.packages.${pkgs.system}.default)
-         (cliphist.overrideAttrs (_old: {
-          src = pkgs.fetchFromGitHub {
-           owner = "sentriz";
-           repo = "cliphist";
-              rev = "c49dcd26168f704324d90d23b9381f39c30572bd";
-              sha256 = "sha256-2mn55DeF8Yxq5jwQAjAcvZAwAg+pZ4BkEitP6S2N0HY=";
-            };
+
+    # Override for cliphis  Waiting for update to reach unstable bracnh
+    #  10-16-24
+    #  https://nixpk.gs/pr-tracker.html?pr=348887
+
+    (cliphist.overrideAttrs (_old: {
+      src = pkgs.fetchFromGitHub {
+       owner = "sentriz";
+       repo = "cliphist";
+       rev = "c49dcd26168f704324d90d23b9381f39c30572bd";
+       sha256 = "sha256-2mn55DeF8Yxq5jwQAjAcvZAwAg+pZ4BkEitP6S2N0HY=";
+       };
         vendorHash = "sha256-M5n7/QWQ5POWE4hSCMa0+GOVhEDCOILYqkSYIGoy/l0=";
       }))
-
-(   cliphist.overrideAttrs (_old: {
-        src = pkgs.fetchFromGitHub {
-         owner = "sentriz";
-         repo = "cliphist";
-         rev = "c49dcd26168f704324d90d23b9381f39c30572bd";
-        sha256 = "sha256-2mn55DeF8Yxq5jwQAjAcvZAwAg+pZ4BkEitP6S2N0HY=";
-        };
-        vendorHash = "sha256-M5n7/QWQ5POWE4hSCMa0+GOVhEDCOILYqkSYIGoy/l0=";
-        }))
 
     alacritty                                                                             
     appimage-run                                                                          
