baaa4c88055febce5e52edc35cfd0e6e6fed366f
Donald Williams <129223418+dwilliam62@users.noreply.github.com>
2024-10-23 13:09:46 -0400

    Ran nixfmt on flake.nix
    Signed-off-by: Donald Williams <129223418+dwilliam62@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -1,43 +1,48 @@
 {
-  description = "KooL's NixOS-Hyprland"; 
-  	
-  inputs = {
-  	nixpkgs.url = "nixpkgs/nixos-unstable";
-	#wallust.url = "git+https://codeberg.org/explosion-mental/wallust?ref=dev";
-	hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1"; # hyprland development
-	distro-grub-themes.url = "github:AdisonCavani/distro-grub-themes"; 
-    wezterm.url = "github:wez/wezterm?dir=nix";
-  	};
+  description = "KooL's NixOS-Hyprland";
 
-  outputs = 
-	inputs@{ self,nixpkgs, wezterm,  ... }:
-    	let
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    #wallust.url = "git+https://codeberg.org/explosion-mental/wallust?ref=dev";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1"; # hyprland development
+    distro-grub-themes.url = "github:AdisonCavani/distro-grub-themes";
+    wezterm.url = "github:wez/wezterm?dir=nix";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      wezterm,
+      ...
+    }:
+    let
       system = "x86_64-linux";
       host = "p520-jakos";
       username = "dwilliams";
-       defaultPackage.x86_64-linux = wezterm.packages.x86_64-linux.default;
+      defaultPackage.x86_64-linux = wezterm.packages.x86_64-linux.default;
 
-    pkgs = import nixpkgs {
-       	inherit system;
-       	config = {
-       	allowUnfree = true;
-       	};
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
       };
     in
-      {
-	nixosConfigurations = {
-      "${host}" = nixpkgs.lib.nixosSystem rec {
-		specialArgs = { 
-			inherit system;
-			inherit inputs;
-			inherit username;
-			inherit host;
-			};
-	   		modules = [ 
-				./hosts/${host}/config.nix 
-				inputs.distro-grub-themes.nixosModules.${system}.default
-				];
-			};
-		};
-	};
+    {
+      nixosConfigurations = {
+        "${host}" = nixpkgs.lib.nixosSystem rec {
+          specialArgs = {
+            inherit system;
+            inherit inputs;
+            inherit username;
+            inherit host;
+          };
+          modules = [
+            ./hosts/${host}/config.nix
+            inputs.distro-grub-themes.nixosModules.${system}.default
+          ];
+        };
+      };
+    };
 }
