b4fab8f6281165e72ff66fa80e1c3040806174b3
Don Williams <Don.e.Williams@gmail.com>
2024-10-31 00:05:50 -0400

    Updated flake
    Signed-off-by: Don Williams <Don.e.Williams@gmail.com>

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     ...
   }: let
     system = "x86_64-linux";
-    host = "p520-jakos";
+    host = "xps15";
     username = "dwilliams";
     defaultPackage.x86_64-linux = wezterm.packages.x86_64-linux.default;
 
