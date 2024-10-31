eb2312ce9a8def978dc8075d310a34d52da9c923
Donald Williams <129223418+dwilliam62@users.noreply.github.com>
2024-10-31 02:59:12 -0400

    Added input for hyprland plugins
    Signed-off-by: Donald Williams <129223418+dwilliam62@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -5,6 +5,11 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     #wallust.url = "git+https://codeberg.org/explosion-mental/wallust?ref=dev";
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1"; # hyprland development
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
+
     distro-grub-themes.url = "github:AdisonCavani/distro-grub-themes";
     wezterm.url = "github:wez/wezterm?dir=nix";
   };
@@ -16,7 +21,7 @@
     ...
   }: let
     system = "x86_64-linux";
-    host = "xps15";
+    host = "p520-jakos";
     username = "dwilliams";
     defaultPackage.x86_64-linux = wezterm.packages.x86_64-linux.default;
 
