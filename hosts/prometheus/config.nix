3b15cf05fbdcd233f0a59a50ab5bd96594537c44
Donald Williams <129223418+dwilliam62@users.noreply.github.com>
2024-11-13 02:14:49 -0500

    Themeing TUIgreeter for prometheus

--- hosts/prometheus/config.nix
+++ hosts/prometheus/config.nix
@@ -1,17 +1,24 @@
 # Main default config
-
-{ config, pkgs, host, username, options, lib, inputs, system, ...}: let
-  
+{
+  config,
+  pkgs,
+  host,
+  username,
+  options,
+  lib,
+  inputs,
+  system,
+  ...
+}: let
   inherit (import ./variables.nix) keyboardLayout;
   python-packages = pkgs.python3.withPackages (
     ps:
       with ps; [
         requests
         pyquery # needed for hyprland-dots Weather script
-        ]
-    );
-  
-  in {
+      ]
+  );
+in {
   imports = [
     ./hardware.nix
     ./users.nix
@@ -23,7 +30,6 @@
     ../../modules/local-hardware-clock.nix
     ../../modules/system.packages.nix
     ../../modules/packages.nix
-   
   ];
 
   # BOOT related stuff
@@ -33,18 +39,18 @@
     kernelParams = [
       "systemd.mask=systemd-vconsole-setup.service"
       "systemd.mask=dev-tpmrm0.device" #this is to mask that stupid 1.5 mins systemd bug
-      "nowatchdog" 
+      "nowatchdog"
       "modprobe.blacklist=sp5100_tco" #watchdog for AMD
       "modprobe.blacklist=iTCO_wdt" #watchdog for Intel
- 	  ];
+    ];
 
     # This is for OBS Virtual Cam Support
-    kernelModules = [ "v4l2loopback" ];
-    extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
-    
-    initrd = { 
-      availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
-      kernelModules = [ ];
+    kernelModules = ["v4l2loopback"];
+    extraModulePackages = [config.boot.kernelPackages.v4l2loopback];
+
+    initrd = {
+      availableKernelModules = ["xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod"];
+      kernelModules = [];
     };
 
     # Needed For Some Steam Games
@@ -52,38 +58,38 @@
     #  "vm.max_map_count" = 2147483642;
     #};
 
-    ## BOOT LOADERS: NOT USE ONLY 1. either systemd or grub  
+    ## BOOT LOADERS: NOT USE ONLY 1. either systemd or grub
     # Bootloader SystemD
     loader.systemd-boot.enable = true;
-  
-    loader.efi = {
-	    #efiSysMountPoint = "/efi"; #this is if you have separate /efi partition
-	    canTouchEfiVariables = true;
-  	  };
 
-    loader.timeout = 1;    
-  			
+    loader.efi = {
+      #efiSysMountPoint = "/efi"; #this is if you have separate /efi partition
+      canTouchEfiVariables = true;
+    };
+
+    loader.timeout = 1;
+
     # Bootloader GRUB
     #loader.grub = {
-	    #enable = true;
-	    #  devices = [ "nodev" ];
-	    #  efiSupport = true;
-      #  gfxmodeBios = "auto";
-	    #  memtest86.enable = true;
-	    #  extraGrubInstallArgs = [ "--bootloader-id=${host}" ];
-	    #  configurationName = "${host}";
-  	  #	 };
+    #enable = true;
+    #  devices = [ "nodev" ];
+    #  efiSupport = true;
+    #  gfxmodeBios = "auto";
+    #  memtest86.enable = true;
+    #  extraGrubInstallArgs = [ "--bootloader-id=${host}" ];
+    #  configurationName = "${host}";
+    #	 };
 
     # Bootloader GRUB theme, configure below
 
     ## -end of BOOTLOADERS----- ##
-  
+
     # Make /tmp a tmpfs
     tmp = {
       useTmpfs = false;
       tmpfsSize = "30%";
-      };
-    
+    };
+
     # Appimage Support
     binfmt.registrations.appimage = {
       wrapInterpreterInShell = false;
@@ -92,8 +98,8 @@
       offset = 0;
       mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
       magicOrExtension = ''\x7fELF....AI\x02'';
-      };
-    
+    };
+
     plymouth.enable = false;
   };
 
@@ -102,7 +108,6 @@
   #  enable = true;
   #  theme = "nixos";
   #};
-
 
   # Extra Module Options
   drivers.amdgpu.enable = false;
@@ -119,7 +124,7 @@
   # networking
   networking.networkmanager.enable = true;
   networking.hostName = "${host}";
-  networking.timeServers = options.networking.timeServers.default ++ [ "pool.ntp.org" ];
+  networking.timeServers = options.networking.timeServers.default ++ ["pool.ntp.org"];
 
   # Set your time zone.
   time.timeZone = "America/New_York";
@@ -140,41 +145,40 @@
   };
 
   nixpkgs.config.allowUnfree = true;
-  
-  programs = {
-	  hyprland = {
-      enable = true;
-		  package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland; #hyprland-git
-		  portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland; # xdphls
-  	  xwayland.enable = true;
-      };
 
-	
-	  waybar.enable = true;
-	  hyprlock.enable = true;
-	  firefox.enable = false;
-	  git.enable = true;
+  programs = {
+    hyprland = {
+      enable = true;
+      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland; #hyprland-git
+      portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland; # xdphls
+      xwayland.enable = true;
+    };
+
+    waybar.enable = true;
+    hyprlock.enable = true;
+    firefox.enable = false;
+    git.enable = true;
     nm-applet.indicator = true;
     neovim.enable = true;
 
-	  thunar.enable = true;
-	  thunar.plugins = with pkgs.xfce; [
-		  exo
-		  mousepad
-		  thunar-archive-plugin
-		  thunar-volman
-		  tumbler
-  	  ];
-	
+    thunar.enable = true;
+    thunar.plugins = with pkgs.xfce; [
+      exo
+      mousepad
+      thunar-archive-plugin
+      thunar-volman
+      tumbler
+    ];
+
     virt-manager.enable = true;
-    
+
     #steam = {
     #  enable = true;
     #  gamescopeSession.enable = true;
     #  remotePlay.openFirewall = true;
     #  dedicatedServer.openFirewall = true;
     #};
-    
+
     xwayland.enable = true;
 
     dconf.enable = true;
@@ -185,33 +189,34 @@
       enable = true;
       enableSSHSupport = true;
     };
-	
   };
 
   users = {
     mutableUsers = true;
   };
 
-  environment.systemPackages = (with pkgs; [
-  # System Packages
+  environment.systemPackages =
+    (with pkgs; [
+      # System Packages
 
-    #waybar  # if wanted experimental next line
-    #(pkgs.waybar.overrideAttrs (oldAttrs: { mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];}))
-  ]) ++ [
-	  python-packages
-  ];
+      #waybar  # if wanted experimental next line
+      #(pkgs.waybar.overrideAttrs (oldAttrs: { mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];}))
+    ])
+    ++ [
+      python-packages
+    ];
 
   # FONTS
   fonts.packages = with pkgs; [
-        # Moved to system.packatges.nix
-        #noto-fonts
-        # fira-code
-        #  noto-fonts-cjk-sans
-        # jetbrains-mono
-        # font-awesome
-        #  terminus_font
-        #(nerdfonts.override {fonts = ["JetBrainsMono"];})
- 	];
+    # Moved to system.packatges.nix
+    #noto-fonts
+    # fira-code
+    #  noto-fonts-cjk-sans
+    # jetbrains-mono
+    # font-awesome
+    #  terminus_font
+    #(nerdfonts.override {fonts = ["JetBrainsMono"];})
+  ];
 
   # Extra Portal Configuration
   xdg.portal = {
@@ -236,98 +241,96 @@
       };
     };
 
- # ... Added as test to help someone
-#   postgresql = {
-#    enable = true;
-#    ensureDatabases = [ "mydatabase" ];
-#    authentication = pkgs.lib.mkOverride 10 ''
-#      #type database  DBuser  auth-method
-#      local all       all     trust
-#    '';
-#  };
+    # ... Added as test to help someone
+    #   postgresql = {
+    #    enable = true;
+    #    ensureDatabases = [ "mydatabase" ];
+    #    authentication = pkgs.lib.mkOverride 10 ''
+    #      #type database  DBuser  auth-method
+    #      local all       all     trust
+    #    '';
+    #  };
 
-    
     greetd = {
       enable = true;
       vt = 3;
       settings = {
         default_session = {
           user = username;
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland"; # start Hyprland with a TUI login manager
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet -r --theme border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red; --time --cmd Hyprland"; # start Hyprland with a TUI login manager
         };
       };
     };
-    
+
     smartd = {
       enable = true;
       autodetect = true;
     };
-    
-	  gvfs.enable = true;
-	  tumbler.enable = true;
 
-	  pipewire = {
+    gvfs.enable = true;
+    tumbler.enable = true;
+
+    pipewire = {
       enable = true;
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
-	    wireplumber.enable = true;
-  	  };
-	
-	  udev.enable = true;
-	  envfs.enable = true;
-	  dbus.enable = true;
+      wireplumber.enable = true;
+    };
 
-	  fstrim = {
+    udev.enable = true;
+    envfs.enable = true;
+    dbus.enable = true;
+
+    fstrim = {
       enable = true;
       interval = "weekly";
-      };
-  
+    };
+
     libinput.enable = true;
 
     rpcbind.enable = true;
     nfs.server.enable = true;
-  
+
     openssh.enable = true;
     flatpak.enable = true;
-	
-  	blueman.enable = true;
-  	
-  	#hardware.openrgb.enable = true;
-  	#hardware.openrgb.motherboard = "amd";
 
-	  fwupd.enable = true;
+    blueman.enable = true;
 
-	  upower.enable = true;
-    
+    #hardware.openrgb.enable = true;
+    #hardware.openrgb.motherboard = "amd";
+
+    fwupd.enable = true;
+
+    upower.enable = true;
+
     gnome.gnome-keyring.enable = true;
-    
+
     printing = {
       enable = false;
       drivers = [
-         pkgs.hplipWithPlugin
+        pkgs.hplipWithPlugin
       ];
     };
-    
+
     avahi = {
       enable = true;
       nssmdns4 = true;
       openFirewall = true;
     };
-    
+
     ipp-usb.enable = true;
-    
+
     #syncthing = {
     #  enable = false;
     #  user = "${username}";
     #  dataDir = "/home/${username}";
     #  configDir = "/home/${username}/.config/syncthing";
     #};
-
   };
-  
+
   systemd.services.flatpak-repo = {
-    path = [ pkgs.flatpak ];
+    path = [pkgs.flatpak];
     script = ''
       flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
     '';
@@ -335,22 +338,22 @@
 
   # zram
   zramSwap = {
-	  enable = true;
-	  priority = 100;
-	  memoryPercent = 30;
-	  swapDevices = 1;
+    enable = true;
+    priority = 100;
+    memoryPercent = 30;
+    swapDevices = 1;
     algorithm = "zstd";
-    };
-
-  powerManagement = {
-  	enable = true;
-	  cpuFreqGovernor = "schedutil";
   };
 
- hardware.sane = {
+  powerManagement = {
     enable = true;
-    extraBackends = [ pkgs.sane-airscan ];
-    disabledDefaultBackends = [ "escl" ];
+    cpuFreqGovernor = "schedutil";
+  };
+
+  hardware.sane = {
+    enable = true;
+    extraBackends = [pkgs.sane-airscan];
+    disabledDefaultBackends = ["escl"];
   };
 
   # Extra Logitech Support
@@ -359,14 +362,14 @@
 
   # Bluetooth
   hardware = {
-  	bluetooth = {
-	    enable = true;
-	    powerOnBoot = true;
-	    settings = {
-		    General = {
-		      Enable = "Source,Sink,Media,Socket";
-		      Experimental = true;
-		    };
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+      settings = {
+        General = {
+          Enable = "Source,Sink,Media,Socket";
+          Experimental = true;
+        };
       };
     };
   };
@@ -407,8 +410,8 @@
         "nix-command"
         "flakes"
       ];
-      substituters = [ "https://hyprland.cachix.org" ];
-      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     };
     gc = {
       automatic = true;
@@ -448,19 +451,19 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
 
- security.sudo = { 
-   enable = true;
-   extraRules = [ 
-    {
-       users = [ "dwilliams" ];
-       commands = [
-         {
-           command = "ALL";
-           options = [ "NOPASSWD" ];
-         }
+  security.sudo = {
+    enable = true;
+    extraRules = [
+      {
+        users = ["dwilliams"];
+        commands = [
+          {
+            command = "ALL";
+            options = ["NOPASSWD"];
+          }
         ];
-     }
-   ];
+      }
+    ];
   };
 
   system.stateVersion = "24.05"; # Did you read the comment?
