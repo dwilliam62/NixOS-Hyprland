a4e67bc65780a836585caff5b717f6a75e5e6e7b
Don Williams <Don.E.Williams@gmail.com>
2024-11-24 21:12:11 -0500

    Updated flake

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@ outputs = inputs @ {
   ...
 }: let
   system = "x86_64-linux";
-  host = "explorer";
+  host = "p520-jakos";
   username = "dwilliams";
   defaultPackage.x86_64-linux = wezterm.packages.x86_64-linux.default;
 
