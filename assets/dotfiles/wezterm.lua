0bbe250b4cca3225a2cbd5a58cf7cb9e3f7fb2ae
Don Williams <Don.E.Williams@gmail.com>
2024-12-12 02:17:00 -0500

    updated wezterm config added to dotfiles

---
+++ assets/dotfiles/wezterm.lua
@@ -0,0 +1,17 @@
+
+local config = {}
+config.enable_wayland = false
+config.font_size = 10
+config.front_end = "WebGpu"
+-- config.color_scheme = 'AdventureTime'
+-- config.color_scheme = 'Advark Blue'
+config.color_scheme = 'Dracula'
+config.use_fancy_tab_bar = true
+config.default_cursor_style = "BlinkingUnderline"
+config.automatically_reload_config = true
+config.window_close_confirmation = "NeverPrompt"
+config.tab_bar_at_bottom = false
+config.window_background_opacity = 0.6
+
+return config
+
