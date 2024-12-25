939c0ff37f943c94fcd5352466dbc72609b1c372
Donald Williams <129223418+dwilliam62@users.noreply.github.com>
2024-12-25 10:32:55 -0500

    updated flake

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@ outputs = inputs @ {
   ...
 }: let
   system = "x86_64-linux";
-  host = "jak-nixos";
+  host = "p520-jakos";
   username = "dwilliams";
   #defaultPackage.x86_64-linux = wezterm.packages.x86_64-linux.default;
 
