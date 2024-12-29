770b4b393e455d19c4c49cba69554346467152b0
installer <installer@gmail.com>
2024-12-29 01:21:07 -0500

    Updated flake

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@ outputs = inputs @ {
   ...
 }: let
   system = "x86_64-linux";
-  host = "p520-jakos";
+  host = "jak-nixos";
   username = "dwilliams";
   #defaultPackage.x86_64-linux = wezterm.packages.x86_64-linux.default;
 
